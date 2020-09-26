7ce81a36fb42eac6c0b4eb4d8ebd514bfc35054e
auvito <aureliusfnvito@gmail.com>
2020-09-26 23:11:06 +0800

    fix 3

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ const express = require('express')
 const hbs = require('hbs')
 const app = express()
 const port = process.env.PORT || 3000
-const forecast = require('forecast')
-const geocode = require('geocode')
+const forecast = require('./forecast')
+const geocode = require('./geocode')
 
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
